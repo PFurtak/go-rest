e3d19fc42966e9f69d970179f07a09df40dec9ae
Patrick Furtak <patrick@patrickfurtak.com>
2020-07-11 13:17:01 -0400

    Created /goodbye handler go-mod

--- main.go
+++ main.go
@@ -12,9 +12,11 @@ func main() {
 
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
 
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
 
 	http.ListenAndServe(":9000", sm)
 }
